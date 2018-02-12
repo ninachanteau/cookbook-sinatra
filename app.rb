9825d8823f55f55c81c81bda5dffe738a77f923e
Nina Chanteau <nina.chanteau@hec.edu>
2018-02-12 11:59:00 +0100

    sinatra

--- app.rb
+++ app.rb
@@ -2,11 +2,28 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+
+set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  'Hakina Matata :)'
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
+end
+
+get '/' do
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
 end
